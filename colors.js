cd59879be0abb418515e0137935579580ec1f90b
JaanMun <yjm7575@naver.com>
2021-03-22 16:45:01 +0900

    File Divide

---
+++ colors.js
@@ -0,0 +1,35 @@
+let Links = {
+  setColor: function (color) {
+    let aList = document.querySelectorAll('a');
+
+    let i = 0;
+    while (i < aList.length) {
+      aList[i].style.color = color;
+      i = i + 1;
+    }
+  },
+};
+let Body = {
+  setColor: function (color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor: function (color) {
+    document.querySelector('body').style.backgroundColor = color;
+  },
+};
+function nightDayHandler(self) {
+  let target = document.querySelector('body');
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
+}
